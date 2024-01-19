868a13eb47c7d276a731807832028514e6f662cf
Fabio Zendhi Nagao <nagaozen@hotmail.com>
2024-01-19 02:54:54 -0300

    feat: leverage jsdoc for intellisense

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+/**
+ * Adds two numbers together.
+ *
+ * @param {number} x - The first number to add.
+ * @param {number} y - The second number to add.
+ * @returns {number} The sum of the two numbers.
+ */
 export function sum (x, y) {
   return x + y
 }
